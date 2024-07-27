a98d9ab0e030318586eb342ebab974a2962847e6
francis-100 <mayiekaf@gmail.com>
2024-07-27 14:01:30 +0300

    static

--- CertVerifyProject/CertVerify/verification/urls.py
+++ CertVerifyProject/CertVerify/verification/urls.py
@@ -21,5 +21,7 @@ urlpatterns = [
     path('signup/', views.user_signup, name='signup'),
 ]
 
+# Serve static files in development
 if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
