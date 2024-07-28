b34f9939fd87667e98665cac06b359d356c213ed
Francis Mayieka <85951839+francis-100@users.noreply.github.com>
2024-07-28 18:28:56 +0300

    Update build_files.sh

--- CertVerifyProject/CertVerify/build_files.sh
+++ CertVerifyProject/CertVerify/build_files.sh
@@ -1,10 +1,2 @@
-#!/bin/bash
-
-# Use the virtual environment provided by Vercel
-source /vercel/path0/venv/bin/activate
-
-# Install dependencies
 pip install -r requirements.txt
-
-# Collect static files
-python manage.py collectstatic --noinput
+python3.9 manage.py collectstatic
